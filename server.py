272f732b7fa4828bc05beb44b5c75a93a38ae8d9
zyf <1254503980@qq.com>
2025-12-23 13:17:03 +0800

    update

--- server.py
+++ server.py
@@ -1414,12 +1414,12 @@ async (data) => {
         'api_name': 'okx_sign',
         'user_name': 'system',
         'source_website': 'https://web3.okx.com/zh-hans/token?hmi=500&pt=1&rb=8&tama=48&utmi=50&vmi=1000',
-        'hijack_js_url': 'https://web3.okx.com/cdn/assets/okfe/util/ont/5.8.35/ont.js',
+        'hijack_js_url': 'https://web3.okx.com/cdn/assets/okfe/util/ont/5.8.36/ont.js',
         'breakpoint_line_num': 0,
-        'breakpoint_col_num': 143091,
+        'breakpoint_col_num': 146424,
         'target_func': """
 async (data) => {
-    return await no.getTokenAndSign({
+    return await oo.getTokenAndSign({
                                 url: data.url,
                                 fetchConfig: data.fetchConfig,
                                 ontConfig: i
